c06e4b4abd778fc3163492ce049e6430268ac5d4
project12349 <158013329+project12349@users.noreply.github.com>
2026-02-28 19:02:16 +0700

    Update config_reader.py

--- school/config_reader.py
+++ school/config_reader.py
@@ -1,11 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr
 
+#класс чтобы читать файл .env
 class Settings(BaseSettings):
   bot_token: SecretStr
   model_config: SettingsConfigDict = SettingsConfigDict(
       env_file='.env',
       env_file_enconding='utf-8'
   )
+
 
 config = Settings()

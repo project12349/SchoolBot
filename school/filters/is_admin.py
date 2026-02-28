d265f94cea390a19936b63a676b2d951f4cb871d
project12349 <158013329+project12349@users.noreply.github.com>
2026-02-28 19:35:55 +0700

    Update is_admin.py

--- school/filters/is_admin.py
+++ school/filters/is_admin.py
@@ -3,12 +3,15 @@ from typing import List
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
 
+#Класс проверяющий админ ли пользователь
 class IsAdmin(BaseFilter):
 
   def __init__(self, user_ids: int | List[int]) -> None:
     self.user_ids = user_ids
-  
+
+  #проверка ID пользователя и ID админа
   async def __call__(self, message: Message) -> bool:
     if isinstance(self.user_ids, int):
       return message.from_user.id == self.user_ids
+
     return message.from_user.id in self.user_ids

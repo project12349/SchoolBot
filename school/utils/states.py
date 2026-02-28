7e754fe69c957a534c276a54b734f7bbd8ea47be
project12349 <158013329+project12349@users.noreply.github.com>
2026-02-28 19:12:56 +0700

    Update states.py

--- school/utils/states.py
+++ school/utils/states.py
@@ -1,8 +1,10 @@
 from aiogram.fsm.state import StatesGroup, State
 
+#класс формы для регистрации
 class Form(StatesGroup):
   klass=State()
   bukva=State()
 
+#класс для сохранение фото расписания
 class Form1(StatesGroup):
   photo=State()
